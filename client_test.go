31fa1d1cfc3dc5779449565def32bdac8ebcc6fc
Callum Choi <cc-cohere@Callums-MacBook-Pro.local>
2022-01-20 18:25:27 -0500

    added builtin tokenize funcitonality & tests

--- client_test.go
+++ client_test.go
@@ -2,6 +2,7 @@ package cohere
 
 import (
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -144,30 +145,33 @@ func TestEmbed(t *testing.T) {
 }
 
 func TestTokenize(t *testing.T) {
-	co, err := CreateClient(apiKey)
-	if err != nil {
-		t.Error(err)
-	}
-
 	t.Run("TokenizeSuccess", func(t *testing.T) {
 		text := "tokenize me!"
 
-		_, err := co.Tokenize("large", TokenizeOptions{
+		res, err := Tokenize(TokenizeOptions{
 			Text: text,
 		})
 		if err != nil {
-			t.Errorf("expected result, go error: %s", err.Error())
+			t.Errorf("Got error: %s", err.Error())
+		}
+		expected := []int64{34160, 974, 514, 34}
+		if !reflect.DeepEqual(expected, res.Tokens) {
+			t.Errorf("Tokenization failed. Expected: %v, Output: %v", res.Tokens, expected)
 		}
 	})
 
 	t.Run("TokenizeEmptyText", func(t *testing.T) {
 		text := ""
 
-		_, err := co.Tokenize("large", TokenizeOptions{
+		res, err := Tokenize(TokenizeOptions{
 			Text: text,
 		})
-		if err == nil {
-			t.Errorf("expected error, got nil")
+		if err != nil {
+			t.Errorf("Got error: %s", err.Error())
+		}
+		expected := []int64{}
+		if !reflect.DeepEqual(expected, res.Tokens) {
+			t.Errorf("Tokenization failed. Expected: %v, Output: %v", res.Tokens, expected)
 		}
 	})
 }
