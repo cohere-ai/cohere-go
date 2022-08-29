18e774e1902b68dbf0c3e8f51387e7f33c1df616
Robert Kozin <robert@robertkozin.com>
2022-08-29 12:59:26 -0400

    omit required

--- generate.go
+++ generate.go
@@ -21,10 +21,10 @@ type GenerateOptions struct {
 	Model string `json:"model,omitempty"`
 
 	// Represents the prompt or text to be completed.
-	Prompt string `json:"prompt"`
+	Prompt string `json:"prompt,omitempty"`
 
 	// Denotes the number of tokens to predict per generation.
-	MaxTokens uint `json:"max_tokens"`
+	MaxTokens uint `json:"max_tokens,omitempty"`
 
 	// optional - The ID of a custom playground preset.
 	Preset string `json:"preset,omitempty"`
