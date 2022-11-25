950aa416bbc0be29e271adb9b14e83fd201445d4
Callum Choi <callum@cohere.ai>
2022-11-25 04:42:00 -0500

    Fix typo

--- embed.go
+++ embed.go
@@ -9,7 +9,7 @@ type EmbedOptions struct {
 
 	// Accepts one of TruncateNone, TruncateLeft and TruncateRight. Determines how inputs over the maximum context length will be handled.
 	// Passing TruncateLeft will discard the left of the input and TruncateRight will discard the right side of the input.
-	// TruncateNone will return an error if the input is too long.
+	// Defaults to TruncateNone, which will return an error if the input is too long.
 	Truncate string `json:"truncate,omitempty"`
 }
 
