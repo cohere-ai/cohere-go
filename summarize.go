10f1697ffdd7259541f068c4164ec56639aa63ac
Michael Kozakov <michael.kozakov@gmail.com>
2023-02-22 10:55:17 +0000

    Add extractiveness + docs

--- summarize.go
+++ summarize.go
@@ -1,17 +1,25 @@
 package cohere
 
 type SummarizeOptions struct {
-	// Texts to identify languages for
+	// Text to summarize
 	Text string `json:"text"`
-	// One of "paragraph" or "bullets"
+	// 'One of `paragraph` or `bullets`, defaults to `paragraph`.
+	// Indicates the style in which the summary will be delivered - in a free form
+	// paragraph or in bullet points.'
 	Format string `json:"format,omitempty"`
-	// One of "short", "medium" or "long"
+	// One of `short`, `medium` or `long`, defaults to `medium`. Indicates the approximate length of the summary.'
 	Length string `json:"length,omitempty"`
-	// Number between 0 and 1 which indicates how much randomness is applied between the generations
+	// One of `low`, `medium` or `high`, defaults to `low`. Controls how close to the original text the summary is.
+	// `high` extractiveness summaries will lean towards reusing sentences verbatim, while `low` extractiveness
+	// summaries will tend to paraphrase more.'
+	Extractiveness string `json:"string,omitempty"`
+	// Ranges from 0 to 5. Controls the randomness of the output. Lower values tend to generate more “predictable” output,
+	// while higher values tend to generate more “creative” output. The sweet spot is typically between 0 and 1.
 	Temperature *float64 `json:"temperature,omitempty"`
-	// Modifies the underlying prompt. Completes the sentence "Generate a summary _"
+	// A free-form instruction for modifying how the summaries get generated. Should complete the sentence "Generate a summary _".
+	// Eg. "focusing on the next steps" or "written by Yoda"
 	AdditionalCommand string `json:"additional_command,omitempty"`
-	// The summarization model to use
+	// Denotes the summarization model to be used. Defaults to the best performing model
 	Model string `json:"model,omitempty"`
 }
 
