d7fd2c3602feffb079dce05290759e9a0d3f8f01
willie3838 <william@cohere.com>
2023-02-23 11:24:01 -0500

    removed ability for users to override

--- client.go
+++ client.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/cohere-ai/tokenizer"
 )
@@ -16,7 +17,7 @@ type Client struct {
 	APIKey  string
 	BaseURL string
 	Client  http.Client
-	Version string
+	version string
 }
 
 const (
@@ -38,9 +39,9 @@ type CheckAPIKeyResponse struct {
 func CreateClient(apiKey string) (*Client, error) {
 	client := &Client{
 		APIKey:  apiKey,
-		BaseURL: "https://api.cohere.ai/",
+		BaseURL: "https://api.cohere.ai",
 		Client:  *http.DefaultClient,
-		Version: "2022-12-06",
+		version: "v1",
 	}
 
 	res, err := client.CheckAPIKey()
@@ -61,7 +62,8 @@ func CreateClient(apiKey string) (*Client, error) {
 // Client methods
 
 func (c *Client) post(endpoint string, body interface{}) ([]byte, error) {
-	url := c.BaseURL + endpoint
+	url := strings.Join([]string{c.BaseURL, c.version, endpoint}, "/")
+
 	buf, err := json.Marshal(body)
 	if err != nil {
 		return nil, err
@@ -75,9 +77,7 @@ func (c *Client) post(endpoint string, body interface{}) ([]byte, error) {
 	req.Header.Set("Authorization", "BEARER "+c.APIKey)
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Request-Source", "go-sdk")
-	if len(c.Version) > 0 {
-		req.Header.Set("Cohere-Version", c.Version)
-	}
+
 	res, err := c.Client.Do(req)
 	if err != nil {
 		return nil, err
@@ -105,7 +105,8 @@ func (c *Client) post(endpoint string, body interface{}) ([]byte, error) {
 }
 
 func (c *Client) CheckAPIKey() ([]byte, error) {
-	url := c.BaseURL + endpointCheckAPIKey
+	url := strings.Join([]string{c.BaseURL, endpointCheckAPIKey}, "/")
+
 	req, err := http.NewRequest("POST", url, http.NoBody)
 	if err != nil {
 		return nil, err
@@ -114,9 +115,7 @@ func (c *Client) CheckAPIKey() ([]byte, error) {
 	req.Header.Set("Authorization", "BEARER "+c.APIKey)
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Request-Source", "go-sdk")
-	if len(c.Version) > 0 {
-		req.Header.Set("Cohere-Version", c.Version)
-	}
+
 	res, err := c.Client.Do(req)
 	if err != nil {
 		return nil, err
